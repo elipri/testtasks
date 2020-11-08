139004eba428ecaa7691c5af52654b8ea79d046c
elipri <Ely@Kasutajas-MBP.lan>
2020-11-08 23:31:05 +0200

    Adding functions

--- Testtasks/Program.cs
+++ Testtasks/Program.cs
@@ -5,44 +5,84 @@ namespace Testtasks
 {
     class Program
     {
+        //Funktsioon massiivide printimiseks
+        private static void Print(int[] mass)
+        {
+            foreach (int i in mass)
+                Console.Write(i);
+            Console.WriteLine();
+        }
+
+        //Funktsioon massiivide printimiseks tagurpidi
+        private static void Printrev(int[] mass)
+        {
+            for (int i = mass.Length-1;i>=0;i--)
+                Console.Write($"{mass[i]}");
+            Console.WriteLine();
+        }
+
+        //Funktsioon massiivide pööramiseks
+        private static void Reverse(int[] mass)
+        {
+            for (int i=0; i<=mass.Length/2; i++)
+            {
+                int ajutine = mass[i];
+                mass[i] = mass[mass.Length -1-i];
+                mass[mass.Length - 1 - i] = ajutine;
+            }
+        }
+
+        //Funktsioon 2D maatriksite elementide printimiseks
+        private static void Printmatrix(int[][] matr)
+        {
+            for (int i = 0; i < matr[0].Length; i++)
+            {
+                for (int j = 0; j < matr[i].Length; j++)
+                {
+                    Console.Write(matr[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //Funktsioon 2D string maatriksite elementide printimiseks
+        private static void Printsmatrix(string[,] smatr)
+        {
+            for (int i = 0; i < smatr.GetLength(0); i++)
+            {
+                for (int j = 0; j < smatr.GetLength(1); j++)
+                {
+                    Console.Write(smatr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("Hello!");
-            //Luua N elemendiga massiiv ning trükkida see välja. Vahetada massiivi väärtuste järjekord vastupidiseks Reverse() funtsiooni kasutamata. Trükkida tulemus.
-            int[] mass = {1,2,3};
-            Console.WriteLine("These are all the elements in the mass array: ");
-            foreach (int i in mass)
-            {
-                Console.WriteLine(i);
-            }
-            int[] massrev = new int[3];
-            int index = 0;
-            for (int i = mass.Length-1; i >= 0; i--)
-            {
-                massrev[index] = mass[i];
-                index++;
-            }
-            Console.WriteLine("These are all the elements in the mass array in reverse order: ");
-            foreach(int r in massrev)
-            {
-                Console.WriteLine(r);
-            }
+            
 
-            //Luua 2D maatriks, algväärtustada see ning trükkida kõik väärtused välja ridade kaupa (iga väärtus eraldi väljundi real).
+            Console.Clear();
+            Console.WriteLine("Ahoi!");
+
+            //Massiivi loomine ja selle tagurpidi printimine ja selle pööramine.
+            int[] mass = new int[4] { 1, 2, 3, 4 };
+            Console.WriteLine("Loodud massiiv:");
+            Print(mass);
+            Console.WriteLine("Massiivi tagurpidi printimine:");
+            Printrev(mass);
+            Console.WriteLine("Massiivi pööramine.");
+            Reverse(mass);
+            Print(mass);
+
+
+
+            //2D maatriks, algväärtustamine ning printimine.
             int[][] matr;
             matr = new int[3][];
             matr[0] = new int[3] { 1, 2, 3 };
             matr[1] = new int[2] { 1, 2 };
             matr[2] = new int[4] { 1, 2, 3, 4 };
-            for (int i=0;i<matr[0].Length;i++)
-            {
-                Console.WriteLine("These are the elements of the {0}. subarray of the 2D int array:",(i+1));
-                for(int j = 0; j < matr[i].Length; j++)
-                {
-                    Console.WriteLine(matr[i][j]);
-                }   
-            }
 
             string[,] smatr;
             smatr = new string[2, 3]
@@ -50,14 +90,13 @@ namespace Testtasks
                 {"a","b","c" },
                 {"d","e","f" }
             };
-            for (int i = 0; i < smatr.GetLength(0); i++)
-            {
-                Console.WriteLine("These are the elements of the {0}. subarray of the 2D string matrix:",(i+1));
-                for (int j=0;j<smatr.GetLength(1);j++)
-                {
-                    Console.WriteLine(smatr[i,j]);
-                }
-            }
+
+            Console.WriteLine("Prindin 2D maatriksi massiivide kaupa:");
+            Printmatrix(matr);
+
+            Console.WriteLine("Prindin string 2D maatriksi massiivide kaupa:");
+            Printsmatrix(smatr);
+
 
             //Luua list, lasta kasutajal väärtused sisestada. Kui enam ei soovi, siis lasta sisestada tühi rida. Trükkida välja kõik väärtused, mis topelt.
             List<int> numbrid = new List<int>();
@@ -109,5 +148,7 @@ namespace Testtasks
                 Console.WriteLine("Bye!");
             }
         }
+
+        
     }
 }
